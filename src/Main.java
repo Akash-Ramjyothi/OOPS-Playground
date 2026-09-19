bc3cf730dc137fc0146e1066db21d4d94d4ecb77
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-19 22:58:38 +0530

    Added Customer data class

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-//        Account bobsAccount = new Account("12345",
-//                1000.00,
-//                "Bob Brown",
-//                "myemail@bob.com",
-//                "(91) 8978 5678");
+        Customer customer = new Customer("Tim", 1000, "tim@email.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail());
 
-        Account bobsAccount = new Account();
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmail());
 
-        System.out.println(bobsAccount.getNumber());
-        System.out.println(bobsAccount.getBalance());
-
-//        bobsAccount.setNumber("12345");
-//        bobsAccount.setBalance(1000.00);
-//        bobsAccount.setCustomerName("Bob Brown");
-//        bobsAccount.setCustomerEmail("myemail@bob.com");
-//        bobsAccount.setCustomerPhone("(91) 1234 5678");
-
-        bobsAccount.withdrawFunds(100.0);
-        bobsAccount.depositFunds(250);
-        bobsAccount.withdrawFunds(50);
-
-        bobsAccount.withdrawFunds(200);
-
-        Account timsAccount = new Account("Tim",
-                "tim@email.com",
-                "12345");
-        System.out.println("Account No: " + timsAccount.getNumber() + " ; name " + timsAccount.getCustomerName());
+        Customer thirdCustomer = new Customer("Joe", "joe@email.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmail());
     }
 }
