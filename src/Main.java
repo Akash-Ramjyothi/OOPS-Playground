c1b3cd03279f190aa72aca398b475958399de02e
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-21 23:56:44 +0530

    Added basic inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            LPAStudent s = new LPAStudent("S92300" + i,
-                    switch (i) {
-                        case 1 -> "Mary";
-                        case 2 -> "Carol";
-                        case 3 -> "Tim";
-                        case 4 -> "Harry";
-                        case 5 -> "Lisa";
-                        default -> "Anonymous";
-                    },
-                    "05/11/1985",
-                    "Java Masterclass");
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
 
-            System.out.println(s);
-        }
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+    }
 
-        Student pojoStudent = new Student("S923006",
-                "Ann",
-                "05/11/1985",
-                "Java Masterclass");
-        LPAStudent recordStudent = new LPAStudent("S923007",
-                "Bill",
-                "05/11/1985",
-                "Java Masterclass");
-
-        System.out.println(pojoStudent);
-        System.out.println(recordStudent);
-        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exa 829");
-        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
-        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
     }
 }
