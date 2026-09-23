c5a49e7107b96cde7bbb73b449d4b53f75f87203
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-23 23:58:34 +0530

    Designed hierarchy

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,11 @@
-public class Main extends Object {
+public class Main {
     public static void main(String[] args) {
-        Student max = new Student("Max", 21);
-        System.out.println(max);
+        Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
 
-        PrimarySchoolStudent jimmy = new PrimarySchoolStudent("Jimmy", 8, "Carole");
-        System.out.println(jimmy);
-    }
-}
-
-class Student {
-    private String name;
-    private int age;
-
-    Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-//    @Override
-//    public String toString() {
-//        return super.toString();
-//    }
-
-    @Override
-    public String toString() {
-        return name + " is " + age;
-//        return "Student{" +
-//                "name='" + name + '\'' +
-//                ", age=" + age +
-//                '}';
-    }
-}
-
-class PrimarySchoolStudent extends Student {
-    private String parentName;
-
-    PrimarySchoolStudent(String name, int age, String parentName) {
-        super(name, age);
-        this.parentName = parentName;
-    }
-
-    @Override
-    public String toString() {
-        return parentName + "'s kid, " + super.toString();
+        Employee joe = new Employee("Joe", "11/11/1990", "03/03/2020");
+        System.out.println(joe);
     }
 }
